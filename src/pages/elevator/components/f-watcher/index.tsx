6cf90a56aae5c7a70720be633af1ab8b06333b96
Anberm <shuangyan_m@hotmail.com>
2020-04-13 18:21:33 +0800

    多摄像头

--- src/pages/elevator/components/f-watcher/index.tsx
+++ src/pages/elevator/components/f-watcher/index.tsx
@@ -1,20 +1,51 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import Decoration11 from '@jiaminghi/data-view-react/es/decoration11';
 
-import Decoration12 from '../keyboard/decoration12';
 import React, { useEffect, useRef, useState } from 'react';
-
+import Decoration12 from '../keyboard/decoration12';
+// declare const navigator: any;
+// 访问用户媒体设备的兼容方法
+// function getUserMedia(constrains: any, success: any, error: any) {
+//   if (navigator.mediaDevices.getUserMedia) {
+//     // 最新标准API
+//     navigator.mediaDevices
+//       .getUserMedia(constrains)
+//       .then(success)
+//       .catch(error);
+//   } else if (navigator.webkitGetUserMedia) {
+//     // webkit内核浏览器
+//     navigator
+//       .webkitGetUserMedia(constrains)
+//       .then(success)
+//       .catch(error);
+//   } else if (navigator.mozGetUserMedia) {
+//     // Firefox浏览器
+//     navagator
+//       .mozGetUserMedia(constrains)
+//       .then(success)
+//       .catch(error);
+//   } else if (navigator.getUserMedia) {
+//     // 旧版API
+//     navigator
+//       .getUserMedia(constrains)
+//       .then(success)
+//       .catch(error);
+//   }
+// }
 export default function FWatcher() {
   const videoRef = useRef<any>();
   const [videoStyle, setVideoStyle] = useState({ width: '360px', height: '360px' });
-  const openVideo = () => {
+  const openVideo = async () => {
+    const devices = (await navigator.mediaDevices.enumerateDevices()).filter(d=>d.kind==='videoinput');
     // 媒体对象
     navigator.getUserMedia(
       {
-        video: true, // 使用摄像头对象
+        video: {
+          deviceId:devices[0].deviceId
+        }, // 使用摄像头对象
         audio: false, // 不适用音频
       },
-      stream => {
+      (stream: any) => {
         if (videoRef.current) {
           videoRef.current.muted = true;
           videoRef.current.autoplay = true;
@@ -22,7 +53,7 @@ export default function FWatcher() {
           videoRef.current.srcObject = stream;
         }
       },
-      error => {
+      (error: any) => {
         // error.code
         console.log(error);
       },
